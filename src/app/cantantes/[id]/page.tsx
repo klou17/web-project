cfb2857f92501f48fe2991cc14f0073f7f13c210
nikol.1704 <nikol.lazarte@540deg.com>
2025-03-14 17:22:14 +0100

    add cantante detail

---
+++ src/app/cantantes/[id]/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { StatusHandler } from '@/components/StateHanlder'
+import Image from 'next/image'
+import { useParams } from 'next/navigation'
+import { useGetSinger } from '../hooks/useGetSinger'
+
+const SingerDetail = () => {
+  const { id } = useParams()
+
+  const { data: singer, isPending, error } = useGetSinger(id as string)
+
+  return (
+    <StatusHandler isLoading={isPending} error={error}>
+      {singer && (
+        <div className={'max-w-3xl mx-auto p-6 '}>
+          <div className={'flex flex-col md:flex-row items-center md:items-start'}>
+            <Image
+              src={singer.photo}
+              alt={singer.stageName}
+              width={200}
+              height={200}
+              className={'rounded-full shadow-lg mb-4 md:mb-0 md:mr-6'}
+            />
+            <div>
+              <h1 className={'text-3xl font-bold text-gray-900 dark:text-white'}>{singer.stageName}</h1>
+              <p className={'text-gray-600 dark:text-gray-300'}>
+                {singer.firstName} {singer.lastName}
+              </p>
+              <p className={'mt-2 text-gray-500 dark:text-gray-400'}>{singer.bio}</p>
+              <p className={'mt-2 text-sm text-gray-400'}>
+                Fecha de nacimiento: {new Date(singer.birthDate).toLocaleDateString()}
+              </p>
+              <span
+                className={`mt-2 inline-block px-4 py-1 rounded-full text-sm font-semibold ${singer.active ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}>
+                {singer.active ? 'Activo' : 'Retirado'}
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+    </StatusHandler>
+  )
+}
+
+export default SingerDetail
--- src/components/StateHanlder.tsx
+++ src/components/StateHanlder.tsx
@@ -9,18 +9,18 @@ interface StatusHandlerProps {
 export const StatusHandler = ({ isLoading, error, children }: StatusHandlerProps) => {
   if (isLoading) {
     return (
-      <div className={"flex flex-col w-full max-w-md mx-auto items-center justify-center h-64"}>
-        <Loader2 className={"w-8 h-8 text-blue-500 animate-spin"} />
-        <p className={"mt-2 text-gray-600 dark:text-gray-300"}>Cargando...</p>
+      <div className={'flex flex-col w-full max-w-md mx-auto items-center justify-center h-64'}>
+        <Loader2 className={'w-8 h-8 text-blue-500 animate-spin'} />
+        <p className={'mt-2 text-gray-600 dark:text-gray-300'}>Cargando...</p>
       </div>
     )
   }
 
   if (error) {
     return (
-      <div className={"flex flex-col items-center justify-center h-64 bg-red-100 dark:bg-red-900 p-4 rounded-lg"}>
-        <AlertCircle className={"w-8 h-8 text-red-600 dark:text-red-400"} />
-        <p className={"mt-2 text-red-600 dark:text-red-400 font-semibold"}>Algo salió mal... Vuelve a intentarlo</p>
+      <div className={'flex flex-col items-center justify-center h-64 bg-red-100 dark:bg-red-900 p-4 rounded-lg'}>
+        <AlertCircle className={'w-8 h-8 text-red-600 dark:text-red-400'} />
+        <p className={'mt-2 text-red-600 dark:text-red-400 font-semibold'}>Algo salió mal... Vuelve a intentarlo</p>
       </div>
     )
   }
